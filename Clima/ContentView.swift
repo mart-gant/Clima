87b695d39552ac2cf869bb40f9438ffe04043043
user917318 <martgant@gmail.com>
2020-03-17 17:26:20 +0000

    Initial Commit

---
+++ Clima/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clima
+//
+//  Created by user917318 on 3/17/20.
+//  Copyright © 2020 user917318. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
